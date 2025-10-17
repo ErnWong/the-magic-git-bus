ad9b0630ce34a1a8e0260b0168b51ee3d67a3137
Ernest Wong <sudo@ernestwong.nz>
2025-10-18 00:33:46 +1300

    Sketch out generator-style script to mount and init git repo

--- build-state.js
+++ build-state.js
@@ -10,7 +10,7 @@ import { V86 } from "./build/libv86.mjs";
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 const V86_ROOT = path.join(__dirname, "./");
-const OUTPUT_FILE = path.join(V86_ROOT, "images/state.bin");
+const OUTPUT_DIR = path.join(V86_ROOT, "images");
 
 var emulator = new V86({
     bios: { url: path.join(V86_ROOT, "bios/seabios.bin") },
@@ -23,31 +23,52 @@ var emulator = new V86({
 
 console.log("Now booting, please stand by ...");
 
-let serial_text = "";
-let booted = false;
-
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+function waitUntilNewTextEndsWith(str)
+{
+    let serial_text = "";
+    while(!serial_text.endsWith(str))
+    {
+        serial_text += yield;
+    }
+}
+
+async function save(filename)
+{
+    emulator.serial0_send("sync;echo 3 >/proc/sys/vm/drop_caches\n");
+    await delay(10 * 1000);
+    const s = await emulator.save_state();
+    const output_file = path.join(OUTPUT_DIR, filename);
+    await fs.writeFile(output_file, new Uint8Array(s));
+    console.log("Saved as " + output_file);
+}
+
+function* script()
+{
+    yield waitUntilNewTextEndsWith("[root@nixos:~]#");
+
+    save("0-login.bin");
+
+    emulator.serial0_send("mkdir ~/repo && echo done-make-repo\n");
+    yield waitUntilNewTextEndsWith("done-make-repo");
+
+    emulator.serial0_send("mount -t 9p -o trans=virtio,version=9p2000.L host9p ~/repo && done-mount-repo\n");
+    yield waitUntilNewTextEndsWith("done-mount-repo");
+
+    emulator.serial0_send("cd repo && git init && echo done-git-init\n");
+    yield waitUntilNewTextEndsWith("done-git-init");
+
+    save("1-git-init.bin");
+
+    emulator.destroy();
+}
+
+const script_state = script();
 
 emulator.add_listener("serial0-output-byte", async function(byte)
 {
     const c = String.fromCharCode(byte);
-    //process.stdout.write(c);
-
-    serial_text += c;
-
-    if(!booted && serial_text.endsWith("localhost:~# "))
-    {
-        booted = true;
-
-        emulator.serial0_send("sync;echo 3 >/proc/sys/vm/drop_caches\n");
-
-        await delay(10 * 1000);
-
-        const s = await emulator.save_state();
-
-        await fs.writeFile(OUTPUT_FILE, new Uint8Array(s));
-        console.log("Saved as " + OUTPUT_FILE);
-
-        emulator.destroy();
-    }
+    process.stdout.write(c);
+    script_state.next(c);
 });
