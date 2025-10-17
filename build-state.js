09bf2a167a7f4a9f6013391431de71ef620997a9
Ernest Wong <sudo@ernestwong.nz>
2025-10-18 00:12:23 +1300

    Copy build-state.js from v86

---
+++ build-state.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+// Based on https://github.com/copy/v86/blob/9735a0eed83a426b3b14cf98f9043a5a9241b142/tools/docker/alpine/build-state.js
+
+import path from "node:path";
+import fs from "node:fs";
+import url from "node:url";
+import { V86 } from "./build/libv86.mjs";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+const V86_ROOT = path.join(__dirname, "./");
+const OUTPUT_FILE = path.join(V86_ROOT, "images/state.bin");
+
+var emulator = new V86({
+    bios: { url: path.join(V86_ROOT, "bios/seabios.bin") },
+    vga_bios: { url: path.join(V86_ROOT, "bios/vgabios.bin") },
+    autostart: true,
+    memory_size: 512 * 1024 * 1024,
+    vga_memory_size: 8 * 1024 * 1024,
+    network_relay_url: "<UNUSED>",
+});
+
+console.log("Now booting, please stand by ...");
+
+let serial_text = "";
+let booted = false;
+
+emulator.add_listener("serial0-output-byte", function(byte)
+{
+    const c = String.fromCharCode(byte);
+    //process.stdout.write(c);
+
+    serial_text += c;
+
+    if(!booted && serial_text.endsWith("localhost:~# "))
+    {
+        booted = true;
+
+        emulator.serial0_send("sync;echo 3 >/proc/sys/vm/drop_caches\n");
+
+        setTimeout(async function ()
+            {
+                const s = await emulator.save_state();
+
+                fs.writeFile(OUTPUT_FILE, new Uint8Array(s), function(e)
+                    {
+                        if(e) throw e;
+                        console.log("Saved as " + OUTPUT_FILE);
+                        emulator.destroy();
+                    });
+            }, 10 * 1000);
+    }
+});
