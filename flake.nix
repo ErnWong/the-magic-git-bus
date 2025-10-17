5faae3fe6f654e23ae68e05bb33c49cedec0ac22
Ernest Wong <sudo@ernestwong.nz>
2025-10-17 23:24:43 +1300

    Try fix broken background and missing cursor in vim

--- flake.nix
+++ flake.nix
@@ -67,6 +67,10 @@
                       colorscheme gruvbox
                       set background=dark
                       set termguicolors
+                      set t_8f=[38;2;%lu;%lu;%lum " Needed in tmux and v86 xtermjs for setting foreground color (or else invisible cursor and buggy background)
+                      set t_8b=[48;2;%lu;%lu;%lum " Needed in tmux and v86 xtermjs for setting background color
+                      set t_ZH=[3m " Italics
+                      set t_ZR=[23m " End italics
                       syntax on
 
                       let g:airline_theme='gruvbox'
