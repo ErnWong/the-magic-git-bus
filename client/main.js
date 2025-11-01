866221b4641b19cd1cbdeda056344a9e35a929a8
Ernest Wong <sudo@ernestwong.nz>
2025-11-01 22:44:05 +1300

    Have buttons to load state

--- client/main.js
+++ client/main.js
@@ -8,3 +8,51 @@ const emulator = new V86({
     serial_container_xtermjs: document.getElementById('vm'),
 });
 window.emulator = emulator;
+
+const states = [
+    {
+        name: '0-login',
+        downloading: true,
+        postLoadCommand: 'clear\n',
+    },
+    {
+        name: '1-git-init',
+        downloading: true,
+        postLoadCommand: 'clear\n',
+    },
+    {
+        name: '2-git-blobs',
+        downloading: true,
+        postLoadCommand: 'clear\n',
+    },
+    {
+        name: '3-git-trees',
+        downloading: true,
+        postLoadCommand: 'clear\n',
+    },
+];
+let restoring_state = false;
+for(const state of states)
+{
+    const button = document.createElement('button');
+    button.textContent = state.name;
+    function update()
+    {
+        button.disabled = state.downloading || restoring_state;
+    }
+    button.disabled = true;
+    (async () => {
+        const buffer = await (await fetch(new URL(`${state.name}.bin`, IMAGES_DIR))).arrayBuffer();
+        button.addEventListener('click', async () => {
+            restoring_state = true;
+            update();
+            await emulator.restore_state(buffer);
+            emulator.serial0_send(state.postLoadCommand);
+            restoring_state = false;
+            update();
+        });
+        state.downloading = false;
+        update();
+    })();
+    document.getElementById("vm").parentElement.appendChild(button);
+}
