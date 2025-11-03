86d317ff8a0614c3c84d086abe21c78dc42e9163
Ernest Wong <sudo@ernestwong.nz>
2025-11-04 00:38:13 +1300

    Set sane shopts for bash scripts

--- server/run.sh
+++ server/run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo Starting server...
 static-web-server --root "$SCRIPT_DIR/.." "$@"
