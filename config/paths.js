bd22c9e05bf060ba878c3206adf897f66168eee1
Ernest Wong <sudo@ernestwong.nz>
2025-11-10 00:52:43 +1300

    Revert "Reapply "WIP: Explore creating 9pfs based image""
    This reverts commit 9958213f1f55b96bb2f381a95980048d5c0c3167.

--- config/paths.js
+++ config/paths.js
@@ -1,4 +1,5 @@
-export const ROOT = new URL('../', import.meta.url);
+const ROOT = new URL('../', import.meta.url);
+
 export const IMAGES_DIR = new URL("images/", ROOT);
 export const BIOS_DIR = new URL("bios/", ROOT);
 export const toV86Url = url => typeof window === 'undefined' && url.protocol === 'file:' ? url.pathname : url.toString();
