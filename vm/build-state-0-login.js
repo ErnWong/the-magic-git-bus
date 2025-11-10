3329d76cd34883a3b5cf8b1388e9b9685900b732
Ernest Wong <sudo@ernestwong.nz>
2025-11-10 13:05:03 +1300

    There is no boot menu to select now

--- vm/build-state-0-login.js
+++ vm/build-state-0-login.js
@@ -3,17 +3,8 @@
 import buildState from "./state-builder.js";
 
 buildState({
-    script: async function*({ emulator, delay, waitUntilNewTextEndsWith, waitUntilPrompt, run, save })
+    script: async function*({ waitUntilNewTextEndsWith, waitUntilPrompt, run, save })
     {
-        // Wait for boot menu
-        await delay(4000);
-
-        // Select fourth menu item in grub menu (to use serial console)
-        console.log('Selecting menu item');
-        const escape = '\x1b[';
-        const down_key = `${escape}B`;
-        emulator.serial0_send(`${down_key}${down_key}${down_key}${down_key}\n`);
-
         yield* waitUntilNewTextEndsWith("(automatic login)");
         yield* waitUntilPrompt();
         yield* run("clear");
